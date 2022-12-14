6324cfa85dbe3634b739df9b36117bcf66f770ae
andresemiliano1 <andresemiliano1@gmail.com>
2022-12-14 10:24:27 +0100

    Initial commit

---
+++ TestMyPolynomial.java
@@ -0,0 +1,12 @@
+public class TestMyPolynomial {
+    public static void main(String[] args) {
+        double[] acoeffs = new double[] {1, 1, 0};
+        MyPolynomial a = new MyPolynomial(acoeffs);
+        System.out.println("the degree of  " + a.toString() + " is " + a.getDegree());
+        System.out.println("when x = 3, " + a.toString() + " = " + String.valueOf(a.evaluate(3.0)));
+        double[] bcoeffs = new double[] {2,1,2,4};
+        MyPolynomial b = new MyPolynomial(bcoeffs);
+        System.out.println(a.toString() + " plus " + b.toString() + " equals " + a.add(b));
+        System.out.println(a.toString() + " times " + b.toString() + " equals " + a.multiply(b));
+    }
+}
